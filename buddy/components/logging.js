6f5ae8e36bc3c0b0b7c8fa08d42d80fa5606c07f
chunlau111191 <72881205+chunlau111191@users.noreply.github.com>
2023-10-07 18:50:57 +0800

    logging function update

--- buddy/components/logging.js
+++ buddy/components/logging.js
@@ -1,18 +1,25 @@
 import { serverUrl } from '../service/dataService';
 
 export const clickLogger = async (type, gender) => {
-    payload = {
-        type: type,
-        gender: gender
+    const payload = {
+        type,
+        gender
     };
+
     try {
-        await fetch(serverUrl + "click_logging", {
+        const response = await fetch(serverUrl + "click_logging", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(payload)
         });
-    } catch (e) { console.log(e) }
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.log('Failed to log click. Status:', response.status, 'Response:', errorData);
+        }
+
+    } catch (e) { console.log("Error logging click:", e) }
 
 };
